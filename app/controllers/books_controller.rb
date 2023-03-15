30d10f9f8923fb50b20f04206f2cb8aceb20a06a
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-03-15 15:03:52 +0000

    [Add]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,12 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @books = @book.user
   end
 
   def edit
