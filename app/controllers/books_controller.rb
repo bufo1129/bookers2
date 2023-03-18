e8d75f6c5699d326b9f3a9ea0bd32437007c7817
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-03-18 06:32:43 +0000

    [Finish]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
 
   def create
     @book = Book.new(book_params)
@@ -47,6 +49,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+      book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
