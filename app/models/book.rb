122f21076c1460b05436067c28f715d97abfbda5
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-04-01 09:36:39 +0000

    [add]favorite

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
 
   # has_one_attached :image
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   # def get_image
   #   unless image.attached?
@@ -13,5 +14,9 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about", as: "about"
   devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:create, :show, :edit, :index, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
